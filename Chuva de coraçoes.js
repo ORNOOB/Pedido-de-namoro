d60e9e278add69f58c1b9016e67ce22399aa50bd
ORNOOB <90430281+ORNOOB@users.noreply.github.com>
2026-01-12 21:51:14 -0300

    Add files via upload

---
+++ Chuva de coraçoes.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const containerCoracoes = document.getElementById("container-coracoes");
+    for (let i = 0; i < 200; i++) {
+        const coracao = document.createElement("div");
+        coracao.className = "coracao";
+        coracao.style.left = Math.random() * window.innerWidth + "px";
+        coracao.style.animationDuration = (Math.random() * 8 + 5) + "s";
+        containerCoracoes.appendChild(coracao);
+    }
+});
+const botaoNao = document.querySelector(".btn-nao");
+
+function fugir(e) {
+    if (e) e.preventDefault();
+
+    const x = (Math.random() - 0.5) * 800;
+    const y = (Math.random() - 0.5) * 500;
+
+    botaoNao.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+botaoNao.addEventListener("mouseenter", fugir);
+
+botaoNao.addEventListener("touchstart", fugir);
+
+botaoNao.addEventListener("click", (e) => {
+    e.preventDefault();
+});
+
+const botaoSim = document.querySelector(".btn-sim");
+    const musica = document.getElementById("musica");
+
+    botaoSim.addEventListener("click", () => {
+    musica.currentTime = 0;
+    musica.play();
+
+    document.getElementById("mensagem").innerText = "Meio que já sabia a resposta! Te amo ❤️";
+    document.getElementById("botoes").style.display = "none";
+});
